f7336fae1033dbd7479d91df5655cb2aaa4f35b8
Omar Gonzalez <vn53x4c@homeoffice.wal-mart.com>
2023-12-16 10:22:23 -0500

    Add login and authentication from Firebase

---
+++ ChatApp/DataManager/RemoteDataManager/AuthenticationManager.swift
@@ -0,0 +1,60 @@
+//
+//  AuthenticationManager.swift
+//  ChatApp
+//
+//  Created by Omar Gonzalez on 16/12/23.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseFirestore
+
+final class AuthenticationManager {
+    // TODO: - Separate this
+    static let shared = AuthenticationManager()
+    private let auth: Auth
+    private let storage: Storage
+    private let fireStore: Firestore
+    
+    private init() {
+        self.auth = Auth.auth()
+        self.storage = Storage.storage()
+        self.fireStore = Firestore.firestore()
+    }
+
+    func createUser(email: String, password: String, imageData: Data?) async throws {
+        do {
+        // TODO: Make this two parallel
+            try await persistImageToStorage(data: imageData, email: email)
+            try await auth.createUser(withEmail: email, password: password)
+        } catch {
+            throw error
+        }
+    }
+    
+    func signInUser(email: String, password: String) async throws {
+        try await auth.signIn(withEmail: email, password: password)
+    }
+    
+    func persistImageToStorage(data: Data?, email: String) async throws {
+        guard let uid = Auth.auth().currentUser?.uid,
+              let data = data else { return }
+        let ref = storage.reference(withPath: uid)
+        
+        // En paralelo estas dos
+        let _ = try await ref.putDataAsync(data)
+        
+        
+        let url = try await ref.downloadURL()
+        
+        try await storeUserInformation(imageProfileURL: url, email: email)
+        print("Image saved successfully")
+    }
+    
+    func storeUserInformation(imageProfileURL: URL, email: String) async throws {
+        guard let uid = auth.currentUser?.uid else { throw URLError(.cancelled) }
+        let userData = ["email" : email, "uid": uid, "profileImageURL": imageProfileURL.absoluteString]
+        try await fireStore.collection("users").document(uid).setData(userData)
+    }
+}
---
+++ ChatApp/Views/LoginScreen/LoginViewModel.swift
@@ -0,0 +1,80 @@
+//
+//  LoginViewModel.swift
+//  ChatApp
+//
+//  Created by Omar Gonzalez on 16/12/23.
+//
+
+import SwiftUI
+import PhotosUI
+
+@MainActor
+final class LoginViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var errorMessage = ""
+    @Published var successMessage = ""
+    @Published var imageSelection: PhotosPickerItem? = nil {
+        didSet {
+            setImage(from: imageSelection)
+        }
+    }
+    @Published private(set) var selectedImage: UIImage? = nil
+    
+    func signUp() {
+        guard !email.isEmpty, !password.isEmpty else {
+            print("No email or password")
+            return
+        }
+        
+        Task {
+            do {
+                try await AuthenticationManager.shared.createUser(email: email,
+                                                                  password: password,
+                                                                  imageData: selectedImage?.jpegData(compressionQuality: 0.5))
+                successMessage = "User registered successfully"
+                email = ""
+                password = ""
+                try? await Task.sleep(nanoseconds: 2_000_000_000)
+                self.successMessage = ""
+            } catch {
+                self.errorMessage = error.localizedDescription
+                try? await Task.sleep(nanoseconds: 2_000_000_000)
+                self.errorMessage = ""
+            }
+        }
+    }
+    
+    func login() {
+        Task {
+            do {
+                try await AuthenticationManager.shared.signInUser(email: email,
+                                                                  password: password)
+                successMessage = "User logged in successfully"
+                try? await Task.sleep(nanoseconds: 2_000_000_000)
+                self.successMessage = ""
+            } catch {
+                self.errorMessage = error.localizedDescription
+                try? await Task.sleep(nanoseconds: 2_000_000_000)
+                self.errorMessage = ""
+            }
+        }
+    }
+    
+    private func setImage(from selection: PhotosPickerItem?) {
+        guard let selection else { return }
+        
+        Task {
+            do {
+                let data = try await selection.loadTransferable(type: Data.self)
+                if let uiImage = UIImage(data: data!) {
+                    selectedImage = uiImage
+                }
+            } catch {
+                self.errorMessage = error.localizedDescription
+                try? await Task.sleep(nanoseconds: 2_000_000_000)
+                self.errorMessage = ""
+            }
+        }
+    }
+}
